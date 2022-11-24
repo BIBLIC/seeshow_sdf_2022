fcd079da65535c8336742ac23d96fc6d1c758ad2
JeiLeee <72813810+JeiLeee@users.noreply.github.com>
2022-11-24 16:33:59 +0900

    v1.2.3

--- SDF26/pg_info.js
+++ SDF26/pg_info.js
@@ -1,4 +1,4 @@
-var total_page = 6;//페이지 수 -> 시나리오마다 total_page 수 바꾸기 
+var total_page = 10;//페이지 수 -> 시나리오마다 total_page 수 바꾸기 
 
 
 //정답 처리할 영역 클래스 명
@@ -25,6 +25,30 @@ const pageClickArea = [
         page: 4,
         correctAnswer: "title_h1",
     },
+    {
+        page: 5,
+        correctAnswer: "title_h1",
+    },
+    {
+        page: 6,
+        correctAnswer: "title_h1",
+    },
+    {
+        page: 7,
+        correctAnswer: "title_h1",
+    },
+    {
+        page: 8,
+        correctAnswer: "title_h1",
+    },
+    {
+        page: 9,
+        correctAnswer: "title_h1",
+    },
+    {
+        page: 10,
+        correctAnswer: "title_h1",
+    }
 ];
 
 const ctrlBtnNames = [
