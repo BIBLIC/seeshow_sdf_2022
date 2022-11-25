b019b8ee450275f2bf37fa04b4b89329dc7767cd
future <hihylee2@gmail.com>
2022-11-25 11:22:01 +0900

    v1.2.8

--- SDF12/pg_info.js
+++ SDF12/pg_info.js
@@ -89,14 +89,14 @@ const modalCont = [
 //**************************+    커서   +*********************************** 
 
 const cursor_pos_1 = document.querySelector('.cursor_pos_1');
-//const cursor_pos_2 = document.querySelector('.cursor_pos_2');
+const cursor_pos_2 = document.querySelector('.cursor_pos_2');
 const cursor_pos_3 = document.querySelector('.cursor_pos_3');
 const cursor_pos_4 = document.querySelector('.cursor_pos_4');
 const cursor_pos_5 = document.querySelector('.cursor_pos_5');
 const cursor_pos_6 = document.querySelector('.cursor_pos_6');
 
 const cursor_wrap_1 = document.querySelector('.cursor_wrap_1');
-// const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
+const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
 const cursor_wrap_3 = document.querySelector('.cursor_wrap_3');
 const cursor_wrap_4 = document.querySelector('.cursor_wrap_4');
 const cursor_wrap_5 = document.querySelector('.cursor_wrap_5');
@@ -104,7 +104,7 @@ const cursor_wrap_6 = document.querySelector('.cursor_wrap_6');
 
 
 cursor_wrap_1.style.zIndex = "100";
-// cursor_wrap_2.style.zIndex = "100";
+cursor_wrap_2.style.zIndex = "100";
 cursor_wrap_3.style.zIndex = "100";
 cursor_wrap_4.style.zIndex = "100";
 cursor_wrap_5.style.zIndex = "100";
@@ -123,17 +123,17 @@ createPopper(cursor_pos_1, cursor_wrap_1, {
         },
     ],
 });
-// createPopper(cursor_pos_2, cursor_wrap_2, {
-//     placement: 'right',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [0,0],
-//             },
-//         },
-//     ],
-// });
+createPopper(cursor_pos_2, cursor_wrap_2, {
+    placement: 'right',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [0,0],
+            },
+        },
+    ],
+});
 createPopper(cursor_pos_3, cursor_wrap_3, {
     placement: 'right',
     modifiers: [
