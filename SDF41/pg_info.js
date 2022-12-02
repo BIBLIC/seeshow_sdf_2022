3f503a249fb6467ff6f29ee16b54c7aeb1b3c3f0
h0wru <69949437+dkanehtjd@users.noreply.github.com>
2022-12-02 10:17:30 +0900

    v1.6.2

--- SDF41/pg_info.js
+++ SDF41/pg_info.js
@@ -56,182 +56,185 @@ const pageClickArea = [
     },
 ];
 
-// const inputAnswer = [
-//     {},
-//     {},
-//     {},
-//     {},
-//     {
-//         page: 4,
-//         inputableAnswer: ""
-//     },
-//     {
-//         page: 5,
-//         inputableAnswer: "감기"
-//     },
-//     {},
-//     {
-//         page: 7,
-//         inputableAnswer: "2022.10.27"
-//     },
-//     {},
-//     {},
-//     {},
-// ];
+const inputAnswer = [
+    {},
+    {
+        page: 1,
+        inputableAnswer: "김토스"
+    },
+    {
+        page: 2,
+        inputableAnswer: ""
+    },
+    {},
+    {
+        page: 4,
+        inputableAnswer: ""
+    },
+    {},
+    {},
+    {
+        page: 7,
+        inputableAnswer: "123456"
+    },
+    {},
+    {},
+    {},
+];
 
-// const modalCont = [
-//     {
-//         page: 0,
-//         instruction: "'시작하기' 버튼을 클릭하세요."
-//     },
-//     {
-//         page: 1,
-//         instruction: "'병원비 돌려받기'를 클릭하세요."
-//     },
-//     {
-//         page: 2,
-//         instruction: "'내 병원비 돌려받기' 버튼을 클릭하세요."
-//     },
-//     {
-//         page: 3,
-//         instruction: "서류를 받았다면 '받았어요' 버튼을 클릭하세요."
-//     },
-//     {
-//         page: 4,
-//         instruction: "'삼성해상화재보험'을 클릭하세요."
-//     },
-//     {
-//         page: 5,
-//         instruction: "주민번호를 입력 후 '다음' 버튼을 클릭하세요."
-//     },
-//     {
-//         page: 6,
-//         instruction: "병원에 방문한 이유를 간단하게 작성 후 '다음'버튼을 클릭하세요."
-//     },
-//     {
-//         page: 7,
-//         instruction: "날짜를 입력한 후 '다음'버튼을 클릭하세요."
-//     },
-//     {
-//         page: 8,
-//         instruction: "병원에서 받은 서류 사진을 올린 후 '다음' 버튼을 클릭하세요."
-//     },
-//     {
-//         page: 9,
-//         instruction: "'입출금 통장'을 클릭하세요."
-//     },
-//     {
-//         page: 10,
-//         instruction: "신청 정보를 확인 후 '버튼'을 클릭하세요."
-//     },
-// ];
-
-
-
-//*******************************************************************************
-//**************************+    커서   +*********************************** 
-
-// const cursor_pos_1 = document.querySelector('.cursor_pos_1');
-// const cursor_pos_2 = document.querySelector('.cursor_pos_2');
-// const cursor_pos_3 = document.querySelector('.cursor_pos_3');
-// const cursor_pos_4 = document.querySelector('.cursor_pos_4');
-// const cursor_pos_5 = document.querySelector('.cursor_pos_5');
-// const cursor_pos_6 = document.querySelector('.cursor_pos_6');
-// const cursor_pos_7 = document.querySelector('.cursor_pos_7');
-// const cursor_pos_8 = document.querySelector('.cursor_pos_8');
-// const cursor_pos_9 = document.querySelector('.cursor_pos_9');
-// const cursor_pos_10 = document.querySelector('.cursor_pos_10');
-
-// const cursor_wrap_1 = document.querySelector('.cursor_wrap_1');
-// const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
-// const cursor_wrap_3 = document.querySelector('.cursor_wrap_3');
-// const cursor_wrap_4 = document.querySelector('.cursor_wrap_4');
-// const cursor_wrap_5 = document.querySelector('.cursor_wrap_5');
-// const cursor_wrap_6 = document.querySelector('.cursor_wrap_6');
-// const cursor_wrap_7 = document.querySelector('.cursor_wrap_7');
-// const cursor_wrap_8 = document.querySelector('.cursor_wrap_8');
-// const cursor_wrap_9 = document.querySelector('.cursor_wrap_9');
-// const cursor_wrap_10 = document.querySelector('.cursor_wrap_10');
+const modalCont = [
+    {
+        page: 0,
+        instruction: "'시작하기' 버튼을 클릭하세요."
+    },
+    {
+        page: 1,
+        instruction: "'병원비 돌려받기'를 클릭하세요."
+    },
+    {
+        page: 2,
+        instruction: "'내 병원비 돌려받기' 버튼을 클릭하세요."
+    },
+    {
+        page: 3,
+        instruction: "서류를 받았다면 '받았어요' 버튼을 클릭하세요."
+    },
+    {
+        page: 4,
+        instruction: "'삼성해상화재보험'을 클릭하세요."
+    },
+    {
+        page: 5,
+        instruction: "주민번호를 입력 후 '다음' 버튼을 클릭하세요."
+    },
+    {
+        page: 6,
+        instruction: "병원에 방문한 이유를 간단하게 작성 후 '다음'버튼을 클릭하세요."
+    },
+    {
+        page: 7,
+        instruction: "날짜를 입력한 후 '다음'버튼을 클릭하세요."
+    },
+    {
+        page: 8,
+        instruction: "병원에서 받은 서류 사진을 올린 후 '다음' 버튼을 클릭하세요."
+    },
+    {
+        page: 9,
+        instruction: "'입출금 통장'을 클릭하세요."
+    },
+    {
+        page: 10,
+        instruction: "신청 정보를 확인 후 '버튼'을 클릭하세요."
+    },
+];
 
 
-// cursor_wrap_1.style.zIndex = "100";
-// cursor_wrap_2.style.zIndex = "100";
-// cursor_wrap_3.style.zIndex = "100";
-// cursor_wrap_4.style.zIndex = "100";
-// cursor_wrap_5.style.zIndex = "100";
-// cursor_wrap_6.style.zIndex = "100";
-// cursor_wrap_7.style.zIndex = "100";
-// cursor_wrap_8.style.zIndex = "100";
-// cursor_wrap_9.style.zIndex = "100";
-// cursor_wrap_10.style.zIndex = "100";
+
+*******************************************************************************
+**************************+    커서   +*********************************** 
+
+const cursor_pos_1 = document.querySelector('.cursor_pos_1');
+const cursor_pos_2 = document.querySelector('.cursor_pos_2');
+const cursor_pos_3 = document.querySelector('.cursor_pos_3');
+const cursor_pos_4 = document.querySelector('.cursor_pos_4');
+const cursor_pos_5 = document.querySelector('.cursor_pos_5');
+const cursor_pos_6 = document.querySelector('.cursor_pos_6');
+const cursor_pos_7 = document.querySelector('.cursor_pos_7');
+const cursor_pos_8 = document.querySelector('.cursor_pos_8');
+const cursor_pos_9 = document.querySelector('.cursor_pos_9');
+const cursor_pos_10 = document.querySelector('.cursor_pos_10');
+
+const cursor_wrap_1 = document.querySelector('.cursor_wrap_1');
+const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
+const cursor_wrap_3 = document.querySelector('.cursor_wrap_3');
+const cursor_wrap_4 = document.querySelector('.cursor_wrap_4');
+const cursor_wrap_5 = document.querySelector('.cursor_wrap_5');
+const cursor_wrap_6 = document.querySelector('.cursor_wrap_6');
+const cursor_wrap_7 = document.querySelector('.cursor_wrap_7');
+const cursor_wrap_8 = document.querySelector('.cursor_wrap_8');
+const cursor_wrap_9 = document.querySelector('.cursor_wrap_9');
+const cursor_wrap_10 = document.querySelector('.cursor_wrap_10');
 
 
-// const { createPopper } = Popper;
-// createPopper(cursor_pos_1, cursor_wrap_1, {
-//     placement: 'right',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [0,0],
-//             },
-//         },
-//     ],
-// });
-// createPopper(cursor_pos_2, cursor_wrap_2, {
-//     placement: 'right',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [-35,100],
-//             },
-//         },
-//     ],
-// });
-// createPopper(cursor_pos_3, cursor_wrap_3, {
-//     placement: 'right',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [70,80],
-//             },
-//         },
-//     ],
-// });
-// //cursor_wrap_4.style.left="60%";
+cursor_wrap_1.style.zIndex = "100";
+cursor_wrap_2.style.zIndex = "100";
+cursor_wrap_3.style.zIndex = "100";
+cursor_wrap_4.style.zIndex = "100";
+cursor_wrap_5.style.zIndex = "100";
+cursor_wrap_6.style.zIndex = "100";
+cursor_wrap_7.style.zIndex = "100";
+cursor_wrap_8.style.zIndex = "100";
+cursor_wrap_9.style.zIndex = "100";
+cursor_wrap_10.style.zIndex = "100";
 
-// createPopper(cursor_pos_4, cursor_wrap_4, {
-//     placement: 'bottom',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [250,10],
-//             },
-//         },
-//     ],
-// });
-// createPopper(cursor_pos_5, cursor_wrap_5, {
-//     placement: 'right',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [25,200],
-//             },
-//         },
-//     ],
-// });
-// createPopper(cursor_pos_6, cursor_wrap_6, {
-//     placement: 'bottom',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [250,10],
-//             },
-//         },
-//     ],
-// });
+
+const { createPopper } = Popper;
+createPopper(cursor_pos_1, cursor_wrap_1, {
+    placement: 'right',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [0,0],
+            },
+        },
+    ],
+});
+createPopper(cursor_pos_2, cursor_wrap_2, {
+    placement: 'right',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [-35,100],
+            },
+        },
+    ],
+});
+createPopper(cursor_pos_3, cursor_wrap_3, {
+    placement: 'right',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [70,80],
+            },
+        },
+    ],
+});
+//cursor_wrap_4.style.left="60%";
+
+createPopper(cursor_pos_4, cursor_wrap_4, {
+    placement: 'bottom',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [250,10],
+            },
+        },
+    ],
+});
+createPopper(cursor_pos_5, cursor_wrap_5, {
+    placement: 'right',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [25,200],
+            },
+        },
+    ],
+});
+createPopper(cursor_pos_6, cursor_wrap_6, {
+    placement: 'bottom',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [250,10],
+            },
+        },
+    ],
+});
