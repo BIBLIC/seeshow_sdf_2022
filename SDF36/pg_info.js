5a315e5aa4706e82f1167ce5763de67708049da1
hunglee2 <hihylee2@gmail.com>
2022-12-09 12:53:02 +0900

    v1.9.5

--- SDF36/pg_info.js
+++ SDF36/pg_info.js
@@ -1,4 +1,4 @@
-var total_page = 13;//페이지 수 -> 시나리오마다 total_page 수 바꾸기 
+var total_page = 14;//페이지 수 -> 시나리오마다 total_page 수 바꾸기 
 
 //오디오 배열 선언, 페이지 수 대로 배열에 mp3 넣기
 const audioArray = [];
@@ -41,16 +41,16 @@ const pageClickArea = [
         correctAnswer: "pg_6_answer"
     },
     {
-        page: 6,
-        correctAnswer: "pg_6_answer"
+        page: 7,
+        correctAnswer: "pg_7_answer"
     },
     {
-        page: 6,
-        correctAnswer: "pg_6_answer"
+        page: 8,
+        correctAnswer: "pg_8_answer"
     },
     {
-        page: 6,
-        correctAnswer: "pg_6_answer"
+        page: 9,
+        correctAnswer: "pg_9_answer"
     },
     {
         page: 10,
@@ -93,10 +93,16 @@ const inputAnswer = [
         inputableAnswer: "자전거판매"
     },
     {},
+    {
+        page: 9,
+        inputableAnswer: "100000"
+    },
     {},
     {},
-    {},
-    {},
+    {
+        page: 12,
+        inputableAnswer: "안국역"
+    },
     {},
 ];
 
@@ -109,54 +115,69 @@ const modalCont = [
     },
     {
         page: 1,
-        instruction: "1"
+        instruction: "'+' 버튼을 눌러주세요."
     },
     {
         page: 2,
-        instruction: "2"
+        instruction: "'내 물건 팔기'를 눌러주세요."
     },
     {
         page: 3,
-        instruction: "3"
+        instruction: "'지금 동네 인증하기' 버튼을 눌러주세요."
     },
     {
         page: 4,
-        instruction: "4"
+        instruction: "'동네인증 완료하기' 버튼을 눌러주세요."
     },
     {
         page: 5,
-        instruction: "5"
+        instruction: "카메라 버튼을 눌러주세요."
     },
     {
         page: 6,
-        instruction: "6"
+        instruction: "자전거 사진을 눌러주세요."
     },
     {
         page: 7,
-        instruction: "7"
+        instruction: "글 제목 입력란에 '자전거 판매'를 입력해주세요."
     },
     {
         page: 8,
-        instruction: "8"
+        instruction: "'스포츠/레저'를 클릭해주세요."
     },
     {
         page: 9,
-        instruction: "9"
+        instruction: "가격 입력란에 '100000'을 숫자로만 입력해주세요."
     },
     {
         page: 10,
-        instruction: "10"
+        instruction: "게시글 내용을 작성하고 '장소 선택' 부분을 눌러주세요."
     },
     {
         page: 11,
-        instruction: "11"
+        instruction: "'선택 완료' 버튼을 눌러주세요." 
     },
     {
         page: 12,
-        instruction: "12"
+        instruction: "입력란에 '안국역'을 입력해주세요."
     },
     {
         page: 13,
+        instruction: "오른쪽 상단의 '완료'를 눌러 게시글 작성을 완료해주세요. "
+    },
+    {
+        page: 14,
         instruction: "마지막 페이지"
     },
 ];
+
+
+const txt_user_input_fill = document.querySelector('.txt_user_input_fill');
+//시작하기 버튼 클릭 후 컨트롤 바 내부 변경
+$('.pg_12_answer').click(function () {
+    txt_user_input_fill.innerText = user_input_txt;
+});
+
+$(document).on("click", "#next_btn", function () {
+    txt_user_input_fill.innerText = user_input_txt;
+});
