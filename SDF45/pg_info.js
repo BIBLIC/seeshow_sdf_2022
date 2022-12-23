0b4b54a63782085afbb4445d412e0dfc59541ba5
hunglee2 <hihylee2@gmail.com>
2022-12-23 12:56:29 +0900

    v2.6.1

--- SDF45/pg_info.js
+++ SDF45/pg_info.js
@@ -7,13 +7,9 @@ for (j = 1; j <= total_page; j++) {
     audioArray.push(audio);
 }
 
-$('#date-picker').datepicker({
-              
-    language: "ko",
-
-})
-
+// -----------------------------------------------------------------------------------------------------------------------
 //정답 처리할 영역 클래스 명
+//correctAnswer 로 지정한 class 외 영역 클릭 시 모달창 출력
 //-> 후에 for 문 써서 pg_n_answer 로 통일하기
 const pageClickArea = [
     {
@@ -56,28 +52,23 @@ const pageClickArea = [
         page: 9,
         correctAnswer: "pg_9_answer"
     },
-    
-    
+
 ];
 
+// -=----------------------------------------------------------------------------------------------------------------------
+//페이지 수만큼 0부터 {}, 공간이 있어야 함
 const inputAnswer = [
     {},
     {},
     {},
     {},
-    {
-        page: 4,
-        inputableAnswer: ""
-    },
-    {
-        page: 5,
-        inputableAnswer: "감기"
-    },
+    {},
     {},
     {
-        page: 7,
-        inputableAnswer: "2022.10.27"
+        page: 6,
+        inputableAnswer: "36"  
     },
+    {},
     {},
     {},
     {},
@@ -87,175 +78,183 @@ const inputAnswer = [
     {},
 ];
 
+
+// -----------------------------------------------------------------------------------------------------------------------
 const modalCont = [
     {
         page: 0,
-        instruction: "'시작하기' 버튼을 클릭하세요."
+        instruction: ""
     },
     {
         page: 1,
-        instruction: "'병원비 돌려받기'를 클릭하세요."
+        instruction: ""
     },
     {
         page: 2,
-        instruction: "'내 병원비 돌려받기' 버튼을 클릭하세요."
+        instruction: ""
     },
     {
         page: 3,
-        instruction: "서류를 받았다면 '받았어요' 버튼을 클릭하세요."
+        instruction: ""
     },
     {
         page: 4,
-        instruction: "'삼성해상화재보험'을 클릭하세요."
+        instruction: ""
     },
     {
         page: 5,
-        instruction: "주민번호를 입력 후 '다음' 버튼을 클릭하세요."
+        instruction: ""
     },
     {
         page: 6,
-        instruction: "병원에 방문한 이유를 간단하게 작성 후 '다음'버튼을 클릭하세요."
+        instruction: ""
     },
     {
         page: 7,
-        instruction: "날짜를 입력한 후 '다음'버튼을 클릭하세요."
+        instruction: ""
     },
     {
         page: 8,
-        instruction: "병원에서 받은 서류 사진을 올리기 위해 '사진 첨부' 버튼을 클릭하세요."
+        instruction: ""
     },
     {
         page: 9,
-        instruction: "'사진첩에서 가져오기' 버튼을 클릭하세요."
-    },
-    {
-        page: 10,
-        instruction: "갤러리에서 서류를 촬영한 사진을 클릭하세요."
-    },
-    {
-        page: 11,
-        instruction: "선택된 사진을 확인 후 '다음' 버튼을 클릭하세요."
-    },
-    {
-        page: 12,
-        instruction: "'입출금 통장'을 클릭하세요."
-    },
-    {
-        page: 13,
-        instruction: "신청 정보를 확인 후 '버튼'을 클릭하세요."
-    },
-    {
-        page: 14,
         instruction: ""
     },
+
 ];
 
 
 
-//*******************************************************************************
-//**************************+    커서   +*********************************** 
+// //*******************************************************************************
+// //**************************+    커서   +*********************************** 
 
-const cursor_pos_1 = document.querySelector('.cursor_pos_1');
-const cursor_pos_2 = document.querySelector('.cursor_pos_2');
-// const cursor_pos_3 = document.querySelector('.cursor_pos_3');
-// const cursor_pos_4 = document.querySelector('.cursor_pos_4');
-// const cursor_pos_5 = document.querySelector('.cursor_pos_5');
-// const cursor_pos_6 = document.querySelector('.cursor_pos_6');
-// const cursor_pos_7 = document.querySelector('.cursor_pos_7');
-// const cursor_pos_8 = document.querySelector('.cursor_pos_8');
-// const cursor_pos_9 = document.querySelector('.cursor_pos_9');
-// const cursor_pos_10 = document.querySelector('.cursor_pos_10');
+// const cursor_pos_1 = document.querySelector('.cursor_pos_1');
+// const cursor_pos_2 = document.querySelector('.cursor_pos_2');
+// // const cursor_pos_3 = document.querySelector('.cursor_pos_3');
+// // const cursor_pos_4 = document.querySelector('.cursor_pos_4');
+// // const cursor_pos_5 = document.querySelector('.cursor_pos_5');
+// // const cursor_pos_6 = document.querySelector('.cursor_pos_6');
 
-const cursor_wrap_1 = document.querySelector('.cursor_wrap_1');
-const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
-// const cursor_wrap_3 = document.querySelector('.cursor_wrap_3');
-// const cursor_wrap_4 = document.querySelector('.cursor_wrap_4');
-// const cursor_wrap_5 = document.querySelector('.cursor_wrap_5');
-// const cursor_wrap_6 = document.querySelector('.cursor_wrap_6');
-// const cursor_wrap_7 = document.querySelector('.cursor_wrap_7');
-// const cursor_wrap_8 = document.querySelector('.cursor_wrap_8');
-// const cursor_wrap_9 = document.querySelector('.cursor_wrap_9');
-// const cursor_wrap_10 = document.querySelector('.cursor_wrap_10');
+// const cursor_wrap_1 = document.querySelector('.cursor_wrap_1');
+// const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
+// // const cursor_wrap_3 = document.querySelector('.cursor_wrap_3');
+// // const cursor_wrap_4 = document.querySelector('.cursor_wrap_4');
+// // const cursor_wrap_5 = document.querySelector('.cursor_wrap_5');
+// // const cursor_wrap_6 = document.querySelector('.cursor_wrap_6');
 
 
-cursor_wrap_1.style.zIndex = "100";
-cursor_wrap_2.style.zIndex = "100";
-// cursor_wrap_3.style.zIndex = "100";
-// cursor_wrap_4.style.zIndex = "100";
-// cursor_wrap_5.style.zIndex = "100";
-// cursor_wrap_6.style.zIndex = "100";
-// cursor_wrap_7.style.zIndex = "100";
-// cursor_wrap_8.style.zIndex = "100";
-// cursor_wrap_9.style.zIndex = "100";
-// cursor_wrap_10.style.zIndex = "100";
+// cursor_wrap_1.style.zIndex = "100";
+// cursor_wrap_2.style.zIndex = "100";
+// // cursor_wrap_3.style.zIndex = "100";
+// // cursor_wrap_4.style.zIndex = "100";
+// // cursor_wrap_5.style.zIndex = "100";
+// // cursor_wrap_6.style.zIndex = "100";
 
 
-const { createPopper } = Popper;
-createPopper(cursor_pos_1, cursor_wrap_1, {
-    placement: 'bottom',
-    modifiers: [
-        {
-            name: 'offset',
-            options: {
-                offset: [150,10],
-            },
-        },
-    ],
-});
-createPopper(cursor_pos_2, cursor_wrap_2, {
-    placement: 'right',
-    modifiers: [
-        {
-            name: 'offset',
-            options: {
-                offset: [55,100],
-            },
-        },
-    ],
-});
-// createPopper(cursor_pos_3, cursor_wrap_3, {
+// const { createPopper } = Popper;
+// createPopper(cursor_pos_1, cursor_wrap_1, {
 //     placement: 'right',
 //     modifiers: [
 //         {
 //             name: 'offset',
 //             options: {
-//                 offset: [70,80],
+//                 offset: [0,0],
 //             },
 //         },
 //     ],
 // });
-// //cursor_wrap_4.style.left="60%";
-
-// createPopper(cursor_pos_4, cursor_wrap_4, {
-//     placement: 'bottom',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [250,10],
-//             },
-//         },
-//     ],
-// });
-// createPopper(cursor_pos_5, cursor_wrap_5, {
+// createPopper(cursor_pos_2, cursor_wrap_2, {
 //     placement: 'right',
 //     modifiers: [
 //         {
 //             name: 'offset',
 //             options: {
-//                 offset: [25,200],
+//                 offset: [30,30],
 //             },
 //         },
 //     ],
 // });
-// createPopper(cursor_pos_6, cursor_wrap_6, {
-//     placement: 'bottom',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [250,10],
-//             },
-//         },
-//     ],
-// });
+// // createPopper(cursor_pos_3, cursor_wrap_3, {
+// //     placement: 'right',
+// //     modifiers: [
+// //         {
+// //             name: 'offset',
+// //             options: {
+// //                 offset: [70,80],
+// //             },
+// //         },
+// //     ],
+// // });
+// // //cursor_wrap_4.style.left="60%";
+
+// // createPopper(cursor_pos_4, cursor_wrap_4, {
+// //     placement: 'bottom',
+// //     modifiers: [
+// //         {
+// //             name: 'offset',
+// //             options: {
+// //                 offset: [250,10],
+// //             },
+// //         },
+// //     ],
+// // });
+// // createPopper(cursor_pos_5, cursor_wrap_5, {
+// //     placement: 'right',
+// //     modifiers: [
+// //         {
+// //             name: 'offset',
+// //             options: {
+// //                 offset: [25,200],
+// //             },
+// //         },
+// //     ],
+// // });
+// // createPopper(cursor_pos_6, cursor_wrap_6, {
+// //     placement: 'bottom',
+// //     modifiers: [
+// //         {
+// //             name: 'offset',
+// //             options: {
+// //                 offset: [250,10],
+// //             },
+// //         },
+// //     ],
+// // });
+
+
+$(document).on("click", '.pg_4_answer', function () {
+    if (page_num === 4) {
+        $( document ).ready(function() {
+            setTimeout(function () {
+                if (page_num === 5) {
+                    next();
+                }
+            }, 800);
+          });  
+    }
+});
+
+$(document).on("click", "#next_btn", function () {
+    if (page_num === 4) {
+        $( document ).ready(function() {
+            setTimeout(function () {
+                if (page_num === 5) {
+                    next();
+                }
+            }, 800);
+          });  
+    }
+});
+
+$(document).on("click", "#prev_btn", function () {
+    if (page_num === 6) {
+        $( document ).ready(function() {
+            setTimeout(function () {
+                if (page_num === 5) {
+                    next();
+                }
+            }, 800);
+          });  
+    }
+});
