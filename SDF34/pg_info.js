b6b1e0b1a92dd82191c70e134fa102b2813f79af
h0wru <69949437+dkanehtjd@users.noreply.github.com>
2022-12-27 12:42:11 +0900

    v2.9.1

---
+++ SDF34/pg_info.js
@@ -0,0 +1,249 @@
+var total_page = 13;//페이지 수 -> 시나리오마다 total_page 수 바꾸기 
+
+//오디오 배열 선언, 페이지 수 대로 배열에 mp3 넣기
+const audioArray = [];
+for (j = 1; j <= total_page; j++) {
+    audio = new Audio(`./assets/audios/${j}.mp3`);
+    audioArray.push(audio);
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+//정답 처리할 영역 클래스 명
+//correctAnswer 로 지정한 class 외 영역 클릭 시 모달창 출력
+//-> 후에 for 문 써서 pg_n_answer 로 통일하기
+const pageClickArea = [
+    {
+        page: 0,
+        correctAnswer: "pg_0_answer"
+    },
+    {
+        page: 1,
+        correctAnswer: "pg_1_answer"
+    },
+    {
+        page: 2,
+        correctAnswer: "pg_2_answer"
+    },
+    {
+        page: 3,
+        correctAnswer: "pg_3_answer"
+    },
+    {
+        page: 4,
+        correctAnswer: "pg_4_answer"
+    },
+    {
+        page: 5,
+        correctAnswer: "pg_5_answer"
+    },
+    {
+        page: 6,
+        correctAnswer: "pg_6_answer"
+    },
+    {
+        page: 7,
+        correctAnswer: "pg_7_answer"
+    },
+    {
+        page: 8,
+        correctAnswer: "pg_8_answer"
+    },
+    {
+        page: 9,
+        correctAnswer: "pg_9_answer"
+    },
+    {
+        page: 10,
+        correctAnswer: "pg_10_answer"
+    },
+    {
+        page: 11,
+        correctAnswer: "pg_11_answer"
+    },
+    {
+        page: 12,
+        correctAnswer: "pg_12_answer"
+    },
+    {
+        page: 13,
+        correctAnswer: "pg_13_answer"
+    },
+];
+
+// -=----------------------------------------------------------------------------------------------------------------------
+//페이지 수만큼 0부터 {}, 공간이 있어야 함
+const inputAnswer = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+];
+
+
+// -----------------------------------------------------------------------------------------------------------------------
+const modalCont = [
+    {
+        page: 0,
+        instruction: "'시작하기' 버튼을 눌러주세요."
+    },
+    {
+        page: 1,
+        instruction: "'다음' 버튼을 눌러주세요."
+    },
+    {
+        page: 2,
+        instruction: "'허용'을 눌러주세요"
+    },
+    {
+        page: 3,
+        instruction: "'바로 시작하기' 버튼을 눌러주세요."
+    },
+    {
+        page: 4,
+        instruction: "좌측 상단의 메뉴를 눌러주세요."
+    },
+    {
+        page: 5,
+        instruction: "'로그인해주세요'를 눌러주세요."
+    },
+    {
+        page: 6,
+        instruction: "'간편 로그인'을 눌러주세요."
+    },
+    {
+        page: 7,
+        instruction: "생년월일 6자리 입력 후 '간편 로그인' 버튼을 눌러주세요."
+    },
+    {
+        page: 8,
+        instruction: "'나의 데이터/통화' 영역을 눌러주세요."
+    },
+    {
+        page: 9,
+        instruction: "'실시간 잔여량' 영역을 눌러주세요."
+    },
+    {
+        page: 10,
+        instruction: "조회된 실시간 잔여량 확인 후 네비게이션 바의 '다음' 버튼을 눌러주세요."
+    },
+    {
+        page: 11,
+        instruction: ""
+    },
+    {
+        page: 12,
+        instruction: ""
+    },
+    {
+        page: 13,
+        instruction: ""
+    },
+];
+
+
+
+// //*******************************************************************************
+// //**************************+    커서   +*********************************** 
+
+const cursor_pos_1 = document.querySelector('.cursor_pos_1');
+const cursor_pos_2 = document.querySelector('.cursor_pos_2');
+// // const cursor_pos_3 = document.querySelector('.cursor_pos_3');
+// // const cursor_pos_4 = document.querySelector('.cursor_pos_4');
+// // const cursor_pos_5 = document.querySelector('.cursor_pos_5');
+// // const cursor_pos_6 = document.querySelector('.cursor_pos_6');
+
+const cursor_wrap_1 = document.querySelector('.cursor_wrap_1');
+const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
+// // const cursor_wrap_3 = document.querySelector('.cursor_wrap_3');
+// // const cursor_wrap_4 = document.querySelector('.cursor_wrap_4');
+// // const cursor_wrap_5 = document.querySelector('.cursor_wrap_5');
+// // const cursor_wrap_6 = document.querySelector('.cursor_wrap_6');
+
+
+cursor_wrap_1.style.zIndex = "100";
+cursor_wrap_2.style.zIndex = "100";
+// // cursor_wrap_3.style.zIndex = "100";
+// // cursor_wrap_4.style.zIndex = "100";
+// // cursor_wrap_5.style.zIndex = "100";
+// // cursor_wrap_6.style.zIndex = "100";
+
+
+const { createPopper } = Popper;
+createPopper(cursor_pos_1, cursor_wrap_1, {
+    placement: 'right',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [0,30],
+            },
+        },
+    ],
+});
+createPopper(cursor_pos_2, cursor_wrap_2, {
+    placement: 'right',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [0,-20],
+            },
+        },
+    ],
+});
+// // createPopper(cursor_pos_3, cursor_wrap_3, {
+// //     placement: 'right',
+// //     modifiers: [
+// //         {
+// //             name: 'offset',
+// //             options: {
+// //                 offset: [70,80],
+// //             },
+// //         },
+// //     ],
+// // });
+// // //cursor_wrap_4.style.left="60%";
+
+// // createPopper(cursor_pos_4, cursor_wrap_4, {
+// //     placement: 'bottom',
+// //     modifiers: [
+// //         {
+// //             name: 'offset',
+// //             options: {
+// //                 offset: [250,10],
+// //             },
+// //         },
+// //     ],
+// // });
+// // createPopper(cursor_pos_5, cursor_wrap_5, {
+// //     placement: 'right',
+// //     modifiers: [
+// //         {
+// //             name: 'offset',
+// //             options: {
+// //                 offset: [25,200],
+// //             },
+// //         },
+// //     ],
+// // });
+// // createPopper(cursor_pos_6, cursor_wrap_6, {
+// //     placement: 'bottom',
+// //     modifiers: [
+// //         {
+// //             name: 'offset',
+// //             options: {
+// //                 offset: [250,10],
+// //             },
+// //         },
+// //     ],
+// // });
