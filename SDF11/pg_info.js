9248fced1ef550df8d1376bfa98852fe51af41ca
hunglee2 <hihylee2@gmail.com>
2022-12-28 16:33:18 +0900

    v3.1.7

--- SDF11/pg_info.js
+++ SDF11/pg_info.js
@@ -171,14 +171,14 @@ const modalCont = [
 
 const cursor_pos_1 = document.querySelector('.cursor_pos_1');
 const cursor_pos_2 = document.querySelector('.cursor_pos_2');
-// const cursor_pos_3 = document.querySelector('.cursor_pos_3');
+const cursor_pos_3 = document.querySelector('.cursor_pos_3');
 // const cursor_pos_4 = document.querySelector('.cursor_pos_4');
 // const cursor_pos_5 = document.querySelector('.cursor_pos_5');
 // const cursor_pos_6 = document.querySelector('.cursor_pos_6');
 
 const cursor_wrap_1 = document.querySelector('.cursor_wrap_1');
 const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
-// const cursor_wrap_3 = document.querySelector('.cursor_wrap_3');
+const cursor_wrap_3 = document.querySelector('.cursor_wrap_3');
 // const cursor_wrap_4 = document.querySelector('.cursor_wrap_4');
 // const cursor_wrap_5 = document.querySelector('.cursor_wrap_5');
 // const cursor_wrap_6 = document.querySelector('.cursor_wrap_6');
@@ -186,7 +186,7 @@ const cursor_wrap_2 = document.querySelector('.cursor_wrap_2');
 
 cursor_wrap_1.style.zIndex = "100";
 cursor_wrap_2.style.zIndex = "100";
-// cursor_wrap_3.style.zIndex = "100";
+cursor_wrap_3.style.zIndex = "100";
 // cursor_wrap_4.style.zIndex = "100";
 // cursor_wrap_5.style.zIndex = "100";
 // cursor_wrap_6.style.zIndex = "100";
@@ -215,18 +215,17 @@ createPopper(cursor_pos_2, cursor_wrap_2, {
         },
     ],
 });
-// createPopper(cursor_pos_3, cursor_wrap_3, {
-//     placement: 'right',
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [70,80],
-//             },
-//         },
-//     ],
-// });
-// //cursor_wrap_4.style.left="60%";
+createPopper(cursor_pos_3, cursor_wrap_3, {
+    placement: 'right',
+    modifiers: [
+        {
+            name: 'offset',
+            options: {
+                offset: [-50,0],
+            },
+        },
+    ],
+});
 
 // createPopper(cursor_pos_4, cursor_wrap_4, {
 //     placement: 'bottom',
