e69a82fa8b8a2187448186d2b023b5ffb6def51a
hunglee2 <hihylee2@gmail.com>
2023-01-04 16:01:49 +0900

    v4.0.7

--- SDF56/pg_handler_dc6.js
+++ SDF56/pg_handler_dc6.js
@@ -6,13 +6,16 @@ const nextBtn = document.getElementById("next");
 
 const ctrlbar = document.querySelector('.ctrlbar');
 
-//const start_btn_wrap = document.querySelector('.start_btn_wrap');
-//const nav_btn_wrap = document.querySelector('.nav_btn_wrap');
-
 const textModal = document.getElementById("textModal");
 const size_btn = document.getElementById("size_btn");
 const start_btn_wrap = document.getElementById("start_btn_wrap");
 $('.nav_btn_wrap').css('display', 'none');
+$('.play').css('display', 'none');
+
+const msg_data = localStorage.getItem('msg');
+const news_data = localStorage.getItem('news');
+const blog_data = localStorage.getItem('blog');
+const community_data = localStorage.getItem('community');
 
 var audio_stat = 1;
 var pwArr = [];
@@ -38,8 +41,8 @@ window.addEventListener("resize", () => {
 
 //페이지 로드 시 전체 동작 코드 (DOM 로딩 후 동작)
 $(document).ready(function () {
-    //$('.nav_btn_wrap').css('display', 'none');
     firstPageLoader();
+    type_done_check();
 });
 
 
@@ -58,304 +61,26 @@ function firstPageLoader() {
 }
 
 
-//*******************************************************************************
-//**************************+    시작하기 버튼   +***********************************  
-
-// //시작하기 버튼 클릭 후 컨트롤 바 내부 변경
-// $('#start-btn').click(function () {
-//     $('.nav_btn_wrap').css('display', 'block');
-//     //$('.nav_btn_wrap').css('display', 'flex');
-//     $('.start_btn_wrap').css('display', 'none');
-//     //$('.nav_btn_wrap').unwrap();
-//     $('.nav_btn_wrap').contents().unwrap();
-
-//     //시작하기 버튼 클릭 후 페이지 1로 전환되게 하기
-//     page_num = 1;
-//     $(".active").removeClass("active");//active 클래스에서 'active'클래스 제거하고 -> 없어도 되는 코드
-//     $("#p" + page_num).addClass("active");//클릭한 곳에 'active' 클래스 추가
-//     $(".pactive").fadeOut(0).promise().done(function () {//pactive -> active 추가된 p?
-//         //promise() -> 자바스크립트 비동기 처리에 사용되는 객체 -> fadeout 완료 후 실행되는 함수
-//         //.toggleClass('추가클래스') //해당 jquery로 가져온 요소를 클릭마다 클래스를 추가하고 삭제.
-//         $(".pactive").toggleClass("pactive");
-//         $("#p" + page_num).toggleClass("pactive");
-//         $(".pactive").fadeIn(0);
-//     });
-//     ctrlbar.style.background = "#E0E0E0";
-//     audioArray[page_num - 1].load();// 해당 페이지 오디오 로딩
-//     audioArray[page_num - 1].play();// 해당 페이지 오디오 재생
-
-//     //---------ctrlbar shadow
-//     if (page_num == 0) {
-//         $(".ctrlbar").removeClass("ctrlbar_shadow");
-//     } else {
-//         $(".ctrlbar").addClass("ctrlbar_shadow");
-//     }
-//     //var number = $('.blog-main').height();
-//     total_height = $('.blog-main').height();
-// });
-
-
-
-//*******************************************************************************
-//**************************+    다음 버튼   +*********************************** 
-
-
-//다음 버튼 : 클릭 시 페이지 이동, 해당 페이지오디오만 재생(배열 인덱스라 페이지 넘버-1) 
-$(document).on("click", "#next_btn", function () {
-    console.log("다음");
-    //$('#next_btn').click(function () {
-    if (page_num < total_page) {
-        page_num += 1;
-        $(".active").removeClass("active");//active 클래스에서 'active'클래스 제거하고 -> 없어도 되는 코드
-        $("#p" + page_num).addClass("active");//클릭한 곳에 'active' 클래스 추가
-        $(".pactive").fadeOut(0).promise().done(function () {//pactive -> active 추가된 p?
-            //promise() -> 자바스크립트 비동기 처리에 사용되는 객체 -> fadeout 완료 후 실행되는 함수
-            //.toggleClass('추가클래스') //해당 jquery로 가져온 요소를 클릭마다 클래스를 추가하고 삭제.
-            $(".pactive").toggleClass("pactive");
-            $("#p" + page_num).toggleClass("pactive");
-            $(".pactive").fadeIn(0);
-        });
+//각 유형 진행 체크
+function type_done_check() {
+    if (msg_data=="done"){
+        $('.msg_btn').css({ "opacity":"0.4"})
     }
-    console.log(page_num);
-    audioArray[page_num - 1].load();
-    audioArray[page_num - 1].play();
-    if (page_num > 1) {
-        audioArray[page_num - 2].pause();
+    if (news_data=="done"){
+        $('.news_btn').css({ "opacity":"0.4"})
     }
-    if (audio_stat == 0) {
-        audioArray[page_num - 1].pause();
-    } else if (audio_stat == 1) {
-        audioArray[page_num - 1].play();
+    if (blog_data=="done"){
+        $('.blog_btn').css({ "opacity":"0.4"})
     }
-
-    //---------ctrlbar shadow
-    if (page_num == 0) {
-        $(".ctrlbar").removeClass("ctrlbar_shadow");
-    } else {
-        $(".ctrlbar").addClass("ctrlbar_shadow");
+    if (community_data=="done"){
+        $('.community_btn').css({ "opacity":"0.4"})
     }
-
-    //자동 스크롤
-    if (page_num == 2) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 0.8;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 0.45;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-    if (page_num == 3) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 0.9;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 0.55;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-    if (page_num == 4) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 1.2;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 0.7;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-    if (page_num == 5) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 1.25;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 0.8;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-    if (page_num == 6) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 1.5;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 1.0;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-
-});
-
-
-//*******************************************************************************
-//**************************+    이전 버튼   +*********************************** 
-
-
-
-//이전 버튼
-$(document).on("click", "#prev_btn", function () {
-    console.log("이전");
-    if (page_num > 0) {
-        page_num -= 1;
-        $(".active").removeClass("active");//active 클래스에서 'active'클래스 제거하고 -> 없어도 되는 코드
-        $("#p" + page_num).addClass("active");//클릭한 곳에 'active' 클래스 추가
-        $(".pactive").fadeOut(0).promise().done(function () {//pactive -> active 추가된 p?
-            //promise() -> 자바스크립트 비동기 처리에 사용되는 객체 -> fadeout 완료 후 실행되는 함수
-            //.toggleClass('추가클래스') //해당 jquery로 가져온 요소를 클릭마다 클래스를 추가하고 삭제.
-            $(".pactive").toggleClass("pactive");
-            $("#p" + page_num).toggleClass("pactive");
-            $(".pactive").fadeIn(0);
-        });
-    }
-    console.log(page_num);
-    if (page_num != 0) {
-        audioArray[page_num - 1].load();
-        audioArray[page_num - 1].play();
-    }
-    audioArray[page_num].pause();
-    if (audio_stat == 0) {
-        audioArray[page_num - 1].pause();
-    } else if (audio_stat == 1) {
-        audioArray[page_num - 1].play();
-    }
-
-     //자동 스크롤
-     if (page_num == 2) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 0.8;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 0.45;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-    if (page_num == 3) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 0.9;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 0.55;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-    if (page_num == 4) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 1.2;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 0.7;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-    if (page_num == 5) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 1.25;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 0.8;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-    if (page_num == 6) {
-        if (textSizeValue == "2") {
-            let scrollPos = total_height * 1.5;
-            $('.blog_main').scrollTop(scrollPos);
-        } else {
-            let scrollPos = total_height * 1.0;
-            $('.blog_main').scrollTop(scrollPos);
-        }
-    }
-   
-   
-
-});
-
-
-
-//*******************************************************************************
-//**************************+    정지 버튼   +*********************************** 
-
-
-$('.play').css('display', 'none');
-
-//정지 버튼
-$(document).on("click", "#pause_btn", function () {
-    $('.stop').css('display', 'none');
-    $('.play').css('display', 'block');
-    $('.play').css('display', 'flex');
-    audioArray[page_num - 1].pause();
-    audio_stat = 0;
-});
-
-//재생 버튼
-$(document).on("click", "#play_btn", function () {
-    $('.stop').css('display', 'block');
-    $('.play').css('display', 'none');
-    $('.stop').css('display', 'flex');
-    audioArray[page_num - 1].play();
-    audio_stat = 1;
-});
-
-
-//*******************************************************************************
-//**************************+    사이즈 버튼   +*********************************** 
-
-
-
-//텍스트 크기 조절 버튼 클릭 시 토글 뜰 수 있게
-var options = {
-    html: true,
-    // title: "Optional: HELLO(Will overide the default-the inline title)",
-    content: $('[data-name="popover-content"]')
-
-}
-
-//토글 안에 슬라이더 넣기
-var tSliderEl = document.getElementById('tSlider')
-var popover = new bootstrap.Popover(tSliderEl, options)
-var slider = document.getElementById("tSliderRange");
-var output = document.getElementById("value");
-
-//슬라이더 바에 맞추어 텍스트 사이즈 조절
-var $txt_area = $(".fontSz");
-//$txt_area.css("background-color","black");
-var currentSize = $txt_area.css("font-size");   /* 폰트사이즈를 알아낸다. */
-var num = parseFloat(currentSize, 10); /* parseFloat()은 숫자가 아니면 숫자가 아니라는 뜻의 NaN을 반환한다. */
-var unit = currentSize.slice(-2);   /* 끝에서부터 두자리의 문자를 가져온다. */
-var textSizeValue = 0;
-//output.innerHTML = slider.value;//슬라이더 값 출력할 곳
-slider.oninput = function () {
-    // output.innerHTML = this.value;
-    textSizeValue = this.value;
-    if (textSizeValue == "0") {
-        //$txt_area.css("fontSize", "1em");
-        $txt_area.attr("style", "font-size: 1em !important");
-    } else if (textSizeValue == "1") {
-        //$txt_area.css("fontSize", "1.2em");
-        $txt_area.attr("style", "font-size: 1.2rem !important");
-    } else if (textSizeValue == "2") {
-        //$txt_area.css("fontSize", "1.4em");
-        $txt_area.attr("style", "font-size: 1.4em !important");
+    if ((blog_data=="done")&&(news_data=="done")&&(blog_data=="done")&&(community_data=="done")){
+        
     }
 }
 
 
-//*******************************************************************************
-//**************************+    힌트 버튼   +*********************************** 
-
-// $(".cursor_wrap").css('display', 'none');
-// $(".pg_" + page_num + "_answer").css('box-shadow', '#00000');
-// $('.shadow_ext').css('box-shadow', '#00000');
-// $('.shadow_inner').css('box-shadow', 'inset #00000');
-
-// //시작하기 버튼 클릭 후 컨트롤 바 내부 변경
-// $('#hint_btn').click(function () {
-//     var myPgNum = $(".active").attr('id');
-//     var real_pg = myPgNum.slice(1);
-//     $(".cursor_wrap_" + real_pg).toggleClass('show');
-//     $(".pg_" + real_pg + "_answer:first").toggleClass("addShadow");
-//     $('.shadow_ext').toggleClass("addShadow");
-//     $(".pg_" + real_pg + "_shadow_inner").toggleClass("addInnerShadow");
-//     $('.border_darker').toggleClass("mk_border_darker");
-// })
 
 //*******************************************************************************
 //**************************+    클릭 제어   +*********************************** 
@@ -377,5 +102,32 @@ function next() {
     if (page_num > 1) {
         audioArray[page_num - 2].pause();
     }
-     
+
 }
+
+// 각 유형 클릭 시 이벤트
+$('.msg_btn').click(function () {
+    localStorage["msg"] = "done";
+    location.href = "./SDF52/SDF52.html";
+});
+
+$('.news_btn').click(function () {
+    localStorage["news"] = "done";
+    location.href = "./SDF53/SDF53.html";
+});
+
+$('.blog_btn').click(function () {
+    localStorage["blog"] = "done";
+    location.href = "./SDF54/SDF54.html";
+});
+
+$('.community_btn').click(function () {
+    localStorage["community"] = "done";
+    location.href = "./SDF55/SDF55.html";
+});
+
+//리프레시 버튼
+$('.refresh_btn').click(function () {
+    window.localStorage.clear();
+    location.reload(true);
+});
